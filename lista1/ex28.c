b749e1b4914b38d8ef49c3aa66e0db5af7299d4d
João Victor <joaovictorpinheiro1510@gmail.com>
2026-08-26 17:08:19 -0300

    fix ex28.c

--- lista1/ex28.c
+++ lista1/ex28.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
+long long mdc(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main(void) {
-    double n;
+    long long input = 0, decimal = 0;
+    long long den = 1;
 
-    scanf("%lf", &n);
+    scanf("%lld", &input);
 
-    int i = 1;
-    while (n * i != (int)(n * i)) {
-        i++;
+    char c = getchar();
+    if (c == '.') {
+        while ((c = getchar()) >= '0' && c <= '9') {
+            decimal = decimal * 10 + (c - '0');
+            den *= 10;
+        }
     }
 
-    printf("%d/%d\n", (int)(n * i), i);
+    long long num = input * den + decimal;
+    long long d = mdc(num, den);
+
+    num /= d;
+    den /= d;
+
+    printf("%lld/%lld\n", num, den);
 
     return 0;
 }
